1a16037fb481b5e9bf5cdee9c484fab81f7028d5
schristou88 <schristou88@gmail.com>
2012-09-17 08:18:11 -0500

    Initial commit.

---
+++ src/main/java/com/thalesgroup/hudson/plugins/xunit/service/XUnitToolInfo.java
@@ -0,0 +1,92 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Thales Corporate Services SAS                             *
+ * Author : Gregory Boissinot                                                   *
+ *                                                                              *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy *
+ * of this software and associated documentation files (the "Software"), to deal*
+ * in the Software without restriction, including without limitation the rights *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell    *
+ * copies of the Software, and to permit persons to whom the Software is        *
+ * furnished to do so, subject to the following conditions:                     *
+ *                                                                              *
+ * The above copyright notice and this permission notice shall be included in   *
+ * all copies or substantial portions of the Software.                          *
+ *                                                                              *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,*
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN    *
+ * THE SOFTWARE.                                                                *
+ *******************************************************************************/
+
+package com.thalesgroup.hudson.plugins.xunit.service;
+
+import com.thalesgroup.dtkit.metrics.model.InputMetric;
+import hudson.FilePath;
+
+import java.io.Serializable;
+
+
+public class XUnitToolInfo implements Serializable {
+
+    private FilePath userContentRoot;
+
+    private final InputMetric inputMetric;
+
+    private final String expandedPattern;
+
+    private final boolean failIfNotNew;
+
+    private final boolean deleteOutputFiles;
+
+    private boolean stopProcessingIfError;
+
+    private final long buildTime;
+
+    private FilePath cusXSLFile;
+
+    public XUnitToolInfo(FilePath userContentRoot, InputMetric inputMetric, String expandedPattern, Boolean failIfNotNew, Boolean deleteOutputFiles, Boolean stopProcessingIfError, long buildTime, FilePath cusXSLFile) {
+        this.userContentRoot = userContentRoot;
+        this.inputMetric = inputMetric;
+        this.expandedPattern = expandedPattern;
+        this.failIfNotNew = failIfNotNew;
+        this.deleteOutputFiles = deleteOutputFiles;
+        this.stopProcessingIfError = stopProcessingIfError;
+        this.buildTime = buildTime;
+        this.cusXSLFile = cusXSLFile;
+    }
+
+    public FilePath getCusXSLFile() {
+        return cusXSLFile;
+    }
+
+    public InputMetric getInputMetric() {
+        return inputMetric;
+    }
+
+    public String getExpandedPattern() {
+        return expandedPattern;
+    }
+
+    public long getBuildTime() {
+        return buildTime;
+    }
+
+    public boolean isFailIfNotNew() {
+        return failIfNotNew;
+    }
+
+    public boolean isDeleteOutputFiles() {
+        return deleteOutputFiles;
+    }
+
+    public boolean isStopProcessingIfError() {
+        return stopProcessingIfError;
+    }
+
+    public FilePath getUserContentRoot() {
+        return userContentRoot;
+    }
+}
---
+++ src/main/java/org/jenkinsci/plugins/xunit/threshold/FailedThreshold.java
@@ -0,0 +1,55 @@
+package org.jenkinsci.plugins.xunit.threshold;
+
+import com.thalesgroup.hudson.plugins.xunit.service.XUnitLog;
+import hudson.model.AbstractBuild;
+import hudson.model.Result;
+import hudson.tasks.junit.TestResultAction;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * @author Gregory Boissinot
+ */
+public class FailedThreshold extends XUnitThreshold {
+
+    public FailedThreshold() {
+    }
+
+    @DataBoundConstructor
+    public FailedThreshold(String unstableThreshold, String unstableNewThreshold, String failureThreshold, String failureNewThreshold) {
+        super(unstableThreshold, unstableNewThreshold, failureThreshold, failureNewThreshold);
+    }
+
+    @Override
+    public Result getResultThresholdNumber(XUnitLog log, AbstractBuild<?, ?> build, TestResultAction testResultAction, TestResultAction previousTestResultAction) {
+
+        int failedCount = testResultAction.getFailCount();
+
+        int previousFailedCount = 0;
+        if (previousTestResultAction != null) {
+            previousFailedCount = previousTestResultAction.getFailCount();
+        }
+        int newFailedCount = failedCount - previousFailedCount;
+
+
+        return getResultThresholdNumber(log, failedCount, newFailedCount);
+    }
+
+    @Override
+    public Result getResultThresholdPercent(XUnitLog log, AbstractBuild<?, ?> build, TestResultAction testResultAction, TestResultAction previousTestResultAction) {
+
+        double count = testResultAction.getTotalCount();
+
+        double failedCount = testResultAction.getFailCount();
+        double percentFailed = (failedCount / count) * 100;
+
+        double previousFailedCount = 0;
+        if (previousTestResultAction != null) {
+            previousFailedCount = previousTestResultAction.getFailCount();
+        }
+        double newFailedCount = failedCount - previousFailedCount;
+        double percentNewFailed = (newFailedCount / count) * 100;
+
+        return getResultThresholdPercent(log, percentFailed, percentNewFailed);
+    }
+
+}
